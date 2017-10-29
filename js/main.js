c556e52ee2974cb5af3d4ce922506329ed3f5ecc
kru1980 <kru71101@gmail.com>
2017-10-29 16:20:56 +0500

    add scroll button

--- js/main.js
+++ js/main.js
@@ -67,4 +67,28 @@ $(document).ready(function () {
 	// 	return false;
 	// });
 
+
+	$(document).on('scroll', function () {
+
+		if ($(window).scrollTop() > 1100) {
+			$('.scroll-top-wrapper').addClass('show');
+		} else {
+			$('.scroll-top-wrapper').removeClass('show');
+		}
+	});
+
+	$('.scroll-top-wrapper').on('click', scrollToTop);
+
+
+	function scrollToTop() {
+		verticalOffset = typeof (verticalOffset) != 'undefined' ? verticalOffset : 0;
+		element = $('body');
+		offset = element.offset();
+		offsetTop = offset.top;
+		$('html, body').animate({
+			scrollTop: offsetTop
+		}, 500, 'linear');
+	}
+
+
 });
